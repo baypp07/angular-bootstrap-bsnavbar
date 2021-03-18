ce7a4ec75538f223914cdead5e9ec6d10593505a
baypp07 <pphouone@gmail.com>
2021-03-18 18:32:36 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, BsNavbarComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
